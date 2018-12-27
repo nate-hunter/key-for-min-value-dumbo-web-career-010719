b07972ca98b68d972d7346a90c8eecc08d024d76
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-27 15:18:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,10 @@ def key_for_min_value(name_hash)
 lowest_key = nil 
 lowest_value = nil
 
-  name_hash.each do |k, val|
+  name_hash.each do |key, val|
     if lowest_value == nil || val < lowest_value
         lowest_value = val
-        lowest_key = k
+        lowest_key = key
       end
   end
   lowest_key
